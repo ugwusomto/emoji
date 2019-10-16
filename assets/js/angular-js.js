2d1c7b9ebf649cb85dfaecd6816c49668a1a8a65
JudeUgwu <38662862+JudeUgwu@users.noreply.github.com>
2019-10-16 13:02:42 +0100

    z

--- assets/js/angular-js.js
+++ assets/js/angular-js.js
@@ -1,15 +1,16 @@
 var app = angular.module("Platzi", []);
 app.controller("BaseCtrl", function($scope, $http) {
-  $http.get("/emoji").then(function(response) {
-    console.log(response.data);
-    $scope.emojis = response.data;
-  });
+  //   $http.get("/emoji").then(function(response) {
+  //     console.log(response.data);
+  //     $scope.emojis = response.data;
+  //   });
 
   // if we want it to be in real time we can ditch the angular $http na use sails js io.socket
-  //   io.socket.get("/emoji", function(data) {
-  //     $scope.emojis = data;
-  //     $scope.$apply();
-  //   });
+  io.socket.get("/emoji", function(data) {
+    console.log(data);
+    $scope.emojis = data;
+    $scope.$apply();
+  });
 
   // When workiing with blueprints sails sails.io sends us events when there is a change in the data
   // base and we update our user interface
