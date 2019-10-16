047e759871d288e910d27b0f99d4d9139545dc9a
JudeUgwu <38662862+JudeUgwu@users.noreply.github.com>
2019-10-16 12:31:03 +0100

    made some differences

--- config/env/production.js
+++ config/env/production.js
@@ -44,8 +44,8 @@ module.exports = {
      *                                                                          *
      ***************************************************************************/
     default: {
-      // adapter: 'sails-mysql',
-      // url: 'mysql://user:password@host:port/database',
+      adapter: "sails-mongo",
+      url: process.env.MONGODB_URI
       //--------------------------------------------------------------------------
       //  /\   To avoid checking it in to version control, you might opt to set
       //  ||   sensitive credentials like `url` using an environment variable.
@@ -106,7 +106,7 @@ module.exports = {
    *                                                                         *
    ***************************************************************************/
   blueprints: {
-    shortcuts: false
+    shortcuts: true
   },
 
   /***************************************************************************
